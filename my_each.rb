427bac65bf816e4094c51a14ea7c3e0aa638e50d
Nick Bazaldua <bazalnick@gmail.com>
2020-08-02 09:44:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,5 @@
 def my_each (array)
+<<<<<<< HEAD
   i = 0
   while i < array.length
     yield (array [i])
@@ -6,3 +7,10 @@ def my_each (array)
   end
 array
 end
+=======
+  array = []
+  counter = i
+  while array.length 
+  # code here
+end
+>>>>>>> 4ed63ce3d07d5b72c000839a1ee75300a7ef4c84
